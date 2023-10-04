e5e169d4d8b58a355f0a9432daf311c1e4605dab
Johann Caspar Zahlmann <johanncasparzahlmann@MacBook-Pro-von-Johann.local>
2023-10-05 00:12:50 +0200

    Updated files

--- pressit_app/views.py
+++ pressit_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.conf import settings
+import openai
 
-# Create your views here.
+def generate_sentence(request):
+    openai.api_key = settings.OPENAI_API_KEY
+    if request.method == 'POST':
+        response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a funny and helpful assistant.",
+            },
+            {"role": "user", "content": "Come up with a funny way of asking 'why did you press that button?'"},
+        ],
+        temperature=1,
+        max_tokens=60,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+        sentence = response.choices[0].message["content"].strip()
+        return render(request, 'pressit/generate.html', {'sentence': sentence})
+    return render(request, 'pressit/generate.html')
