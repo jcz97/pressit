c6e7e226b68053fb39be01226376867e7012422b
Johann Caspar Zahlmann <johanncasparzahlmann@MacBook-Pro-von-Johann.local>
2023-10-05 00:58:56 +0200

    Some configuration for deploy

--- pressit/settings.py
+++ pressit/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-an_^qgz1oc^xtm_6bvbdz-!@=8rlk1x8j0ur5lu5_=y6n2zt5m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,6 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
