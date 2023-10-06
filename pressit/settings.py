73fcb12cbcd56f3b3cfdf6fe67a0ac14ad0e33f3
Johann Caspar Zahlmann <johanncasparzahlmann@MacBook-Pro-von-Johann.local>
2023-10-06 10:19:37 +0200

    Css debugging

--- pressit/settings.py
+++ pressit/settings.py
@@ -123,15 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/staticfiles/"
+STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn/")
 
-"""
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "pressit", "static"),
 ]
-"""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
